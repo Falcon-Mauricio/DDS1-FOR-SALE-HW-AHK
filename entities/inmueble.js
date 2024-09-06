4f7f28d913892fb41a988a78ea2b2ce4a71227eb
Mauricio Falcon <105568570+Falcon-Mauricio@users.noreply.github.com>
2024-09-06 07:37:25 -0300

    Actualizar inmueble.js

--- entities/inmueble.js
+++ entities/inmueble.js
@@ -1,1 +1,13 @@
-console.log("INMUEBLE")
+
+class Propiedad {
+  constructor(id, tipo, tamaño, ambientes, valor, zona) {
+    this.id = id;
+    this.tipo = tipo;
+    this.tamaño = tamaño;
+    this.ambientes = ambientes;
+    this.valor = valor;
+    this.zona = zona;
+    this.estado = "disponible"; // disponible, reservada, alquilada, vendida
+    this.clienteReserva = null;
+  }
+}
