d99c251aee45760cd51dbbdda7511a7c4f0531f1
Mauricio Falcon <105568570+Falcon-Mauricio@users.noreply.github.com>
2024-09-06 07:35:13 -0300

    entidad empleado creada

---
+++ entities/empleado.js
@@ -0,0 +1,23 @@
+class Empleado {
+  constructor(id, nombre, apellido) {
+    this.id = id;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.cantidadOperaciones = 0;
+    this.cantidadReservas = 0;
+    this.totalComisiones = 0;
+    this.operaciones = [];
+    this.reservas = [];
+  }
+
+  agregarOperacion(operacion) {
+    this.operaciones.push(operacion);
+    this.cantidadOperaciones += 1;
+    this.totalComisiones += operacion.comision;
+  }
+
+  agregarReserva(reserva) {
+    this.reservas.push(reserva);
+    this.cantidadReservas += 1;
+  }
+}
